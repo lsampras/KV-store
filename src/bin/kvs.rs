3666dd63f3f7191d0ab3a33f05f2e79cffda4eac
sampras lopes <lsampras@protonmail.com>
2021-10-27 23:32:39 +0530

    update the cli interface with KVstore lib

--- src/bin/kvs.rs
+++ src/bin/kvs.rs
@@ -1,7 +1,7 @@
 use clap::{crate_authors, crate_description};
 use structopt::StructOpt;
 use std::process::exit;
-
+use kvs::{KvStore, error::KVResult};
 #[derive(StructOpt)]
 #[structopt(
     name = "Key-Value Server",
@@ -41,23 +41,29 @@ pub enum Command {
 	},
 }
 
+fn run_kv_command(command: Command) -> KVResult<()> {
+	let mut store = KvStore::new()?;
+	Ok(match command {
+		Command::Set{key, value} => store.set(key, value)?,
+		Command::Get{key} => println!("{}", store.get(key)?),
+		Command::Delete{key} => store.remove(key)?,
+	})
+}
+
 #[allow(unused)]
 fn main() {
 	let opt = Opt::from_args();
 	// println!("{:?}, {:?}", opt.data, opt.cmd.unwrap());
 	match opt.cmd {
-		Some(Command::Set{key, value}) => {
-			unimplemented!();
-		},
-		Some(Command::Get{key}) => {
-			unimplemented!();
-		},
-		Some(Command::Delete{key}) => {
-			unimplemented!();
+		Some(command) => {
+			match run_kv_command(command) {
+				Err(e) => {println!("Error occured: {:?}", e); exit(1);},
+				Ok(_) => {},
+			}
 		},
 		None => {
 			eprintln!("Specify a param");
 			exit(1);
 		},
-	}
+	};
 }
